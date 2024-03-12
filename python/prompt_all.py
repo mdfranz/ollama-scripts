a354c6dda6d6874fe41b939e7db3e00a6e29caa6
Matt Franz <mdfranz@gmail.com>
2024-03-12 12:15:34 +0000

    getting stats, not working

--- python/prompt_all.py
+++ python/prompt_all.py
@@ -2,9 +2,17 @@
 
 import ollama
 
+# global so we can use it as a title for CSV
+STAT_FIELDS = ['model','total_duration','load_duration','prompt_eval_duration','eval_count','eval_duration']
+
+def get_stats_row(r)
+  """Extracts interesting model execution stats and puts in a line for CSV"""
+  row = []
+  for s in STAT_FIELDS:
+    row.append(r[s])
+
 if __name__ == "__main__":
   print ("Getting models!") 
-
   p = input("What is your question>")
 
   for m in  ollama.list()['models']:
